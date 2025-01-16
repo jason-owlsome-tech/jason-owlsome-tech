47d23784271967d661246970a0ecdc1bde2b3313
jason-owlsome-tech <jasonhuang@owlsome.tech>
2025-01-16 14:55:18 +0800

    update project settings for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  trailingSlash: true,
+  assetPrefix: isProd ? "/jason-owlsome-tech/" : "",
+  basePath: isProd ? "/jason-owlsome-tech" : "",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
